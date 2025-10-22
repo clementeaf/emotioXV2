378a346459ac5d25f6be710eb2f546d82e8d3d4a
clementeaf <carriagadafalcone@gmail.com>
2025-10-22 08:29:29 -0300

    REFACTOR: Centralizado TrashIcon en /components/common/icons para reutilización

--- frontend/src/components/research/CognitiveTask/components/questions/ChoiceQuestion.tsx
+++ frontend/src/components/research/CognitiveTask/components/questions/ChoiceQuestion.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import { Button } from '@/components/ui/Button';
 import { FormField, FormSection, FormCard, FormRow } from '@/components/common/atomic';
+import { TrashIcon } from '@/components/common/icons';
 import { ChoiceQuestionProps } from '../../types';
-
-// Ícono de papelera simplificado
-const TrashIcon = () => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    width="16" 
-    height="16" 
-    viewBox="0 0 24 24" 
-    fill="none" 
-    stroke="currentColor" 
-    strokeWidth="2" 
-    strokeLinecap="round" 
-    strokeLinejoin="round"
-  >
-    <path d="M3 6h18"></path>
-    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
-  </svg>
-);
 
 /**
  * Componente refactorizado que usa componentes atómicos
